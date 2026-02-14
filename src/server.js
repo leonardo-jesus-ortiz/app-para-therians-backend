bcbbbb96dd5c9f28128c1bc73d08c33c135811ee
Jesus <jesus.l.ortiz11@gmail.com>
2026-02-13 22:58:59 -0300

    feat: reorganizar estructura del servidor y configurar conexión a la base de datos

--- src/server.js
+++ src/server.js
@@ -1,7 +1,7 @@
 import express from 'express';
 import dotenv from 'dotenv';
 import cors from 'cors';
-import connectDB from './config/db.js';
+import { connectDB } from './config/bd.js';
 
 // 1. Configuración
 dotenv.config();
